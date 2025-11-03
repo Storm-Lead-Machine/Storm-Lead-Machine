97409c28b30aa9a204a4fbfb1ecab73891b3f912
Storm-Lead-Machine <Stormleadmachine@gmail.com>
2025-11-02 23:25:13 -0600

    Create Rules.jsx

---
+++ src/pages/Rules.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Rules() {
+  return (
+    <section className="rules reveal">
+      <h4>Rules to Running the Appointments</h4>
+      <ol>
+        <li><b>Do not call the leads ahead of time</b> before you go.</li>
+        <li>
+          <b>Bad leads must be returned within 2 days to receive credit; otherwise we automatically give 20% extra to help make up for bad leads.</b>
+        </li>
+        <li>
+          We get you on the roof in front of the owner in the ZIP codes you want — <b>you close the deal</b>.
+        </li>
+      </ol>
+      <div className="rules-cta">
+        <a className="btn cta" href="https://square.link/u/RSfgAZHS" target="_blank" rel="noreferrer">Get Leads Now</a>
+      </div>
+    </section>
+  );
+}
