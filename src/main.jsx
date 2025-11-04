6b35bc9407e0498adb27877ae0f8e042cd1c6fbf
Storm-Lead-Machine <Stormleadmachine@gmail.com>
2025-11-03 23:11:57 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import "./site.css"; // ✅ only this CSS import
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./site.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
