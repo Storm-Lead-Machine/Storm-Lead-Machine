1a4f48c0b7105a0af769319196e17c1bf39d1850
Storm-Lead-Machine <Stormleadmachine@gmail.com>
2025-11-07 15:35:44 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./site.css";
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
