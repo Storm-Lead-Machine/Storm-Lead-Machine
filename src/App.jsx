744860683af322dfd1ccca4d496dfa6e2b07d3c0
Storm-Lead-Machine <Stormleadmachine@gmail.com>
2025-11-24 11:44:40 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Hail from "./Hail";
-import Wind from "./Wind";
-import Tornado from "./Tornado";
-import Pricing from "./Pricing";
-import Contact from "./Contact";
+import React from "react";
+import Home from "./Home.jsx";
 import "./site.css";
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/hail" element={<Hail />} />
-        <Route path="/wind" element={<Wind />} />
-        <Route path="/tornado" element={<Tornado />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
-  );
+  return <Home />;
 }
+
